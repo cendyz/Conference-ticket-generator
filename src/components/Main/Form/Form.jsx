aeba618698d809562bcd6ccb3e4693f5823343ac
cendyz <arek57823@gmail.com>
2024-12-21 18:15:15 +0100

    form update

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,9 +1,43 @@
 import styles from './Form.module.scss'
-import classNames from 'classnames'
+import { useState } from 'react'
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
 const Form = () => {
+	const [user, setUser] = useState({
+		name: '',
+		email: '',
+		username: '',
+	})
+
+	const handleChange = e => {
+		setUser({ ...user, [e.target.name]: e.target.value })
+	}
+
+	const checkEmail = email => {
+		return emailRegex.test(email)
+	}
+
+	const checkNames = (name, username) => {
+		return name !== '' && username !== ''
+	}
+
+	const handleSubmit = e => {
+		e.preventDefault()
+
+		const formData = new FormData(e.currentTarget)
+		const newUser = Object.fromEntries(formData)
+
+		if (
+			checkEmail(newUser.email) &&
+			checkNames(newUser.name, newUser.username)
+		) {
+			setUser(newUser)
+		}
+	}
+
 	return (
-		<form className={styles.form}>
+		<form className={styles.form} onClick={handleSubmit}>
 			<label htmlFor='avatar' className={styles.avatarLabel}>
 				Upload Avatar
 			</label>
@@ -18,7 +52,13 @@ const Form = () => {
 						Drag and drop or click to upload
 					</span>
 				</label>
-				<input id='avatar' className={styles.avatarInput} type='file' />
+				<input
+					id='avatar'
+					className={styles.avatarInput}
+					type='file'
+					name='avatar'
+					accept='image/png, image/jpeg'
+				/>
 			</div>
 			<div className={styles.avatarErrorInfo}>
 				<img
@@ -33,16 +73,36 @@ const Form = () => {
 			<label htmlFor='fullName' className={styles.formLabel}>
 				Full Name
 			</label>
-			<input type='text' className={styles.formInput} id='fullName' />
+			<input
+				type='text'
+				className={styles.formInput}
+				id='fullName'
+				name='name'
+				onChange={handleChange}
+				value={user.name}
+			/>
 			<label htmlFor='email' className={styles.formLabel}>
 				Email Address
 			</label>
 			<input
-				type='text'
+				type='email'
 				className={styles.formInput}
 				id='email'
 				placeholder='example@email.com'
+				name='email'
+				onChange={handleChange}
+				value={user.email}
 			/>
+			<div className={styles.errorBox}>
+				<img
+					src='src/images/icon-info.svg'
+					alt='Error icon'
+					className={styles.errorIcon}
+				/>
+				<p className={styles.errorText}>
+					Please enter a valid email address.
+				</p>
+			</div>
 			<label htmlFor='username' className={styles.formLabel}>
 				Github Username
 			</label>
@@ -51,8 +111,14 @@ const Form = () => {
 				className={styles.formInput}
 				id='username'
 				placeholder='@yourusername'
+				name='username'
+				onChange={handleChange}
+				value={user.username}
 			/>
-			<button type='submit' className={styles.submitBtn}>Generate My Ticket</button>
+
+			<button type='submit' className={styles.submitBtn}>
+				Generate My Ticket
+			</button>
 		</form>
 	)
 }
