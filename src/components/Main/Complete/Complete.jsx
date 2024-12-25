fb3033a87f498e9ead99bb0408b9115d5737c215
cendyz <arek57823@gmail.com>
2024-12-25 17:26:05 +0100

    Left Ticket

--- src/components/Main/Complete/Complete.jsx
+++ src/components/Main/Complete/Complete.jsx
@@ -1,21 +1,31 @@
 import styles from './Complete.module.scss'
 import { useGlobalContext } from '../../../Context'
+import Ticket from './Ticket'
 
 const Complete = () => {
 	const { user } = useGlobalContext()
 	return (
-		<section className={styles.section}>
-			<img src='src/images/logo-full.svg' alt='' className={styles.logo} />
-			<h2 className={styles.title}>
-				Congrats, <span className={styles.name}>Jonatan Kirstof!</span>{' '}
-				Your ticket is ready.
-			</h2>
-			<p className={styles.desc}>
-				We've emailed your ticket to{' '}
-				<span className={styles.email}>{user.email}</span> and will send
-				updates in the run up to the event.
-			</p>
-		</section>
+		<>
+			<section className={styles.section}>
+				<img
+					src='src/images/logo-full.svg'
+					alt=''
+					className={styles.logo}
+				/>
+				<h2 className={styles.title}>
+					Congrats, <span className={styles.name}>Jonatan Kirstof!</span>{' '}
+					Your ticket is ready.
+				</h2>
+				<p className={styles.desc}>
+					We've emailed your ticket to{' '}
+					<span className={styles.email}>
+						{user.email || 'jonatan@email.com'}
+					</span>{' '}
+					and will send updates in the run up to the event.
+				</p>
+			</section>
+			<Ticket />
+		</>
 	)
 }
 export default Complete
---
+++ src/components/Main/Complete/Ticket/Ticket.jsx
@@ -0,0 +1,35 @@
+import styles from './Ticket.module.scss'
+
+const Ticket = () => {
+  return (
+		<section className={styles.section}>
+			<div className={styles.ticket}>
+				<div className={styles.leftPart}>
+					<div className={styles.leftUp}>
+						<img
+							src='src/images/logo-mark.svg'
+							alt='Logo'
+							className={styles.logo}
+						/>
+						<div className={styles.eventInfo}>
+							<h3 className={styles.eventName}>Coding Conf</h3>
+							<p className={styles.eventDate}>Jan 31, 2025 / Austin, TX</p>
+						</div>
+					</div>
+					<div className={styles.leftDown}>
+						<img
+							src='src/images/image-avatar.jpg'
+							alt='Avatar'
+							className={styles.avatar}
+						/>
+						<div className={styles.personInfo}>
+							<h3 className={styles.personName}>Jonatan Kristof</h3>
+							<p className={styles.personGit}><img src="src/images/icon-github.svg" alt="Github icon" className={styles.gitIcon} /> @jonatankristof0101</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
+export default Ticket
