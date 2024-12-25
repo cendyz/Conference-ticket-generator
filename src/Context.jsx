82f9116ef864267418c6f4eb5ef4e9d88c72b688
cendyz <arek57823@gmail.com>
2024-12-25 20:15:59 +0100

    LocalStorage ok

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,23 +1,33 @@
-import { useState, createContext, useContext } from 'react'
+import { useState, createContext, useContext, useEffect } from 'react'
 
 const GlobalContext = createContext()
 
+const setLocalStorage = ticketNumber => {
+	localStorage.setItem('tickets', JSON.stringify(ticketNumber))
+}
+
 const AppContext = ({ children }) => {
+	const defaultValues = JSON.parse(localStorage.getItem('tickets')) || 1609
 	const [completeForm, setCompleteForm] = useState(false)
 	const [preview, setPreview] = useState(null)
-	const [ticketNumber, setTicketNumber] = useState(1609)
+	const [ticketNumber, setTicketNumber] = useState(defaultValues)
 
 	const [user, setUser] = useState({
 		name: '',
 		email: '',
 		username: '',
 	})
+
+	useEffect(() => {
+		setLocalStorage(ticketNumber)
+	}, [ticketNumber])
+
 	const handleComplete = () => {
 		setCompleteForm(true)
 	}
 
 	const handleTicketNumber = () => {
-		setTicketNumber(ticketNumber + 1)
+		setTicketNumber(prev => prev + 1)
 	}
 
 	return (
